b5d6cf30837225b6f7cc699114dbdefe3e153c10
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-05-16 17:30:46 +0300

    Lab 3 tests done

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,46 @@
+import lab.model.Country;
+import lab.model.Person;
+import lab.model.UsualPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloWorldTest {
+
+	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
+
+	private UsualPerson expectedPerson;
+
+	private BeanFactory context;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+		expectedPerson = getExpectedPerson();
+	}
+
+	@Test
+	void testInitPerson() {
+		Person person = context.getBean("person", Person.class);
+		assertEquals(expectedPerson, person);
+		System.out.println(person);
+	}
+
+	private UsualPerson getExpectedPerson() {
+		UsualPerson person = new UsualPerson();
+		person.setAge(35);
+		person.setName("John Smith");
+
+		Country country = new Country();
+		country.setId(1);
+		country.setName("Russia");
+		country.setCodeName("RU");
+
+		person.setCountry(country);
+
+		return person;
+	}
+}
